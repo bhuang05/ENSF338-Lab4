c36f4382b2ce75dc29927ecee4031d812bca6eb5
bhuang05 <braden.huang@ucalgary.ca>
2024-02-27 23:01:59 -0700

    Finished ex4

--- ex4.1.py
+++ ex4.1.py
@@ -4,7 +4,8 @@
 #a complexity of O(n^2) because the loops are going through all the values in the list twice. 
 # The average case complexity is also O(n^2), assuming that a uniformly distributed list is passed through. This is because it will always go through
 #all elements in the list at least one time which is O(n), but we can assume that on average around half of the list has elements that are greater than five
-#leading to the second loop because activated. We can combine the cases to have a total time complexity of O(n^2/2), but due to big O notation simplifies to just O(n^2)
+#leading to the second loop because activated. We can combine the cases to have a total time complexity of O(n^2/2), but due to big O notation simplifies to just O(n^2).
+#Or we could just add the best and worst cases together and divide them by 2 which would give us (n^2 + n)/2 which simplifies to n^2 so it has complexity O(n^2)
 
 #2.
 def processdata(li):
